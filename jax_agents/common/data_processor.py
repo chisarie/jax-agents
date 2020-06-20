b0ccecac9a06fbf82f965e26d9c4f09a24bca3e7
eugenio_tuxedolaptop <eugenio.chisari@gmail.com>
2020-06-20 18:00:38 +0200

    data_processor

--- jax_agents/common/data_processor.py
+++ jax_agents/common/data_processor.py
@@ -21,23 +21,73 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+"""Processing data stream from interactions."""
+
 from collections import deque
+import numpy as np
+
+
+class ReplayBuffer():
+    """A simple FIFO experience replay buffer for off-policy agents."""
+
+    def __init__(self, buffer_size, state_dim, action_dim):
+        """Initialize replay buffer with zeros."""
+        self.states = np.zeros([buffer_size, state_dim], dtype=np.float32)
+        self.actions = np.zeros([buffer_size, action_dim], dtype=np.float32)
+        self.rewards = np.zeros(buffer_size, dtype=np.float32)
+        self.next_states = np.zeros([buffer_size, state_dim], dtype=np.float32)
+        self.ptr, self.size, self.buffer_size = 0, 0, buffer_size
+        return
+
+    def store(self, state, action, reward, next_state):
+        """Store new experience."""
+        self.states[self.ptr] = state
+        self.actions[self.ptr] = action
+        self.rewards[self.ptr] = reward
+        self.next_states[self.ptr] = next_state
+        self.ptr = (self.ptr+1) % self.buffer_size
+        self.size = min(self.size+1, self.buffer_size)
+        return
+
+    def sample_batch(self, batch_size):
+        """Sample past experience."""
+        indexes = np.random.randint(low=0, high=self.size, size=batch_size)
+        return dict(states=self.states[indexes],
+                    actions=self.actions[indexes],
+                    rewards=self.rewards[indexes],
+                    final_states=self.next_states[indexes])
 
 
 class DataProcessor:
     """Class to process the data stream of states and actions.
 
-    It calculates the rewards and stores 3 tuples (state, action, reward) in a
+    Calculate the rewards and store 3 tuples (state, action, reward) in a
     deque in order to support multistep reinforcement learning
-    (see https://arxiv.org/pdf/1901.07510.pdf).
-
-    :param reward_func: (func) reward_func(state, action) -> reward
-    :param n_steps: (int) how many steps to consider for the multistep setting,
-        use 1 for standard setting
+    (see https://arxiv.org/pdf/1901.07510.pdf). Then fill the replay buffer
+    for off policy rl algorithms.
     """
 
-    def __init__(self, reward_func, n_steps):
-        """Initialize reward function and the multistep deque."""
-        self.reward_func = reward_func
+    def __init__(self, n_steps, buffer_size, state_dim, action_dim):
+        """Initialize the multistep deque and the replay buffer."""
         self.n_steps_deque = deque(maxlen=n_steps+1)
+        self.replay_buffer = ReplayBuffer(buffer_size, state_dim, action_dim)
+        return
+
+    def data_callback(self, state, action, reward_func, reset_flag):
+        """Fill the deque and the replay buffer."""
+        if len(self.n_steps_deque) == 0:
+            self.n_steps_deque.append((state, action, 0.0))
+            return
+        reward = reward_func(self.n_steps_deque[-1][0],  # previous state
+                             self.n_steps_deque[-1][1],  # previous action
+                             state)
+        cum_reward = self.n_steps_deque[-1][2] + reward
+        self.n_steps_deque.append((state, action, cum_reward))
+        if len(self.n_steps_deque) == self.n_steps_deque.maxlen:
+            self.replay_buffer.store(self.n_steps_deque[0][0],  # first state
+                                     self.n_steps_deque[0][1],  # first action
+                                     cum_reward - self.n_steps_deque[0][2],
+                                     state)
+        if reset_flag:
+            self.n_steps_deque.clear
         return
